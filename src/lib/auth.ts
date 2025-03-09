c6a6b1a88b1fce9a7d2523a04d6b5cbd2329d009
Mostafa Reda <moszaki1234@gmail.com>
2025-03-09 17:51:29 +0200

    use postgresql db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,7 @@ import { prisma } from "./prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "mysql",
+    provider: "postgresql",
   }),
 
   emailAndPassword: {
